0f417364469e6221a98a73ce2eb28fb5ddde0a35
Lucas-Github-23 <p.lucas23@escola.pr.gov.br>
2025-04-03 19:13:01 -0300

    Update khan.js

--- api/khan.js
+++ api/khan.js
@@ -1,15 +1,15 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
-export default function (req, res) {
-    const proxy = createProxyMiddleware({
-        target: 'https://www.khanacademy.org',
-        changeOrigin: true,
-        pathRewrite: { '^/api/khan': '' },
-        onProxyRes: (proxyRes) => {
-            delete proxyRes.headers['x-frame-options'];
-            delete proxyRes.headers['content-security-policy'];
-        },
-    });
+const proxy = createProxyMiddleware({
+    target: 'https://www.khanacademy.org',
+    changeOrigin: true,
+    pathRewrite: { '^/api/khan': '' },
+    onProxyRes: (proxyRes) => {
+        delete proxyRes.headers['x-frame-options'];
+        delete proxyRes.headers['content-security-policy'];
+    },
+});
 
+export default function handler(req, res) {
     return proxy(req, res);
 }
